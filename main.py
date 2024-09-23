3c7ab41dd288ed9510d62d5bacbbe50d3918a6e2
Jennacuti <jmcuti@loyola.edu>
2024-09-23 19:49:09 -0400

    program that takes birth rate, death rate , immigrant rate , current population and years into future and ouputs the population change.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# Enter Code here
+# Read the README - under files on left
+# Delete these three lines before submitting
 birth_rate = float(input("Enter the time between births (in seconds):"))
 death_rate = float(input("Enter the time between deaths (in seconds):"))
 immigrant_rate = float(input("Enter the time between immigrants (in seconds):"))
@@ -17,6 +20,3 @@ else:
     change = "remained the same"
 print(f"In {years_into_future} years, the population will be {int(future_population)}.")
 print(f"The population has {change}.")
-
-
-
