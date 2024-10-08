51224884dd828ccac7db4a8bcc2254583c84c815
maxrice <mxrice@loyola.edu>
2024-10-07 20:15:55 -0400

    changed algorithm.docx and main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ birth_rate = float(input("Enter the time between births (in seconds):"))
 death_rate = float(input("Enter the time between deaths (in seconds):"))
 immigrant_rate = float(input("Enter the time between immigrants (in seconds):"))
 current_population = float(input("Enter the current population (in population):"))
-years_into_future = int(input("Enter the number of years into the future:"))
+years_into_future = float(input("Enter the number of years into the future:"))
 SECONDS_PER_YEAR = 365 * 24 * 60 * 60
 births_per_year = SECONDS_PER_YEAR / birth_rate
 deaths_per_year = SECONDS_PER_YEAR / death_rate
@@ -18,5 +18,5 @@ elif future_population < current_population:
     change = "decreased"
 else:
     change = "remained the same"
-print(f"In {years_into_future} years, the population will be {int(future_population)}.")
+print(f"In {years_into_future} years, the population will be {future_population}.")
 print(f"The population has {change}.")
